cbd840d2bb8d535cfb61901aaec2ad4f2eec9728
jamesqquick <james.q.quick@outlook.com>
2020-02-12 11:43:04 -0600

    Adding login with auth0

--- app.js
+++ app.js
@@ -24,6 +24,61 @@ const highScores = document.getElementById('highScores');
 
 let highScoresArray = [];
 
+//Auth Stuff!
+let auth0 = null;
+
+window.onload = async () => {
+    auth0 = await createAuth0Client({
+        domain: 'jqq-intervie-test.auth0.com',
+        client_id: 'nXHTUKU8xb0bie5NPgj8kQI8nt5mk3Wi'
+    });
+
+    updateUI();
+
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        // show the gated content
+        return;
+    }
+
+    // NEW - check for the code and state parameters
+    const query = window.location.search;
+    if (query.includes('code=') && query.includes('state=')) {
+        // Process the login state
+        await auth0.handleRedirectCallback();
+
+        updateUI();
+
+        // Use replaceState to redirect the user away and remove the querystring parameters
+        window.history.replaceState({}, document.title, '/');
+    }
+};
+
+const logout = () => {
+    auth0.logout({
+        returnTo: window.location.origin
+    });
+};
+
+const updateUI = async () => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        document.getElementById('btn-logout').classList.remove('hidden');
+        document.getElementById('btn-login').classList.add('hidden');
+    } else {
+        document.getElementById('btn-logout').classList.add('hidden');
+        document.getElementById('btn-login').classList.remove('hidden');
+    }
+};
+
+const login = async () => {
+    await auth0.loginWithRedirect({
+        redirect_uri: window.location.origin
+    });
+};
+
 //Firebase
 var firebaseConfig = {
     apiKey: 'AIzaSyAm5n0b88hjyRiUBYQYXj2pku5f_W9jvhg',
@@ -100,7 +155,7 @@ const startGame = () => {
 
 const resetGameState = () => {
     score = 0;
-    seconds = 30;
+    seconds = 3;
     ms = 0;
 };
 
